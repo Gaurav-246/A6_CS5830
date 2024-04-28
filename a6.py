d3bdf6fa4091ee12c992550a6022a37ae06b4f14
Gaurav-246 <gauravnvn246@gmail.com>
2024-04-28 18:18:26 +0530

    task2 done

--- a6.py
+++ a6.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 import io
 import matplotlib.pyplot as plt
+import cv2
 
 # Task 1 - Q1
 app = FastAPI()
@@ -30,16 +31,28 @@ def predict_digit(model: Sequential, data_point: list):
     return str(np.argmax(pred))
 
 
+# Task 2 - Q1,Q2,Q3
+
+
+def format_image(contents, size=28, interpolation=cv2.INTER_AREA):
+    with io.BytesIO(contents) as f:
+        img = plt.imread(f)
+    img = img[:,:,:3]
+    resized = cv2.resize(
+        img, (size, size), interpolation=interpolation
+    )  # Resize the image
+    resized = cv2.bitwise_not(resized)
+    data = np.array(resized)
+    data = np.mean(data, axis=2) # Convert to grayscale
+    data = data.flatten()  # Serialize to an array of 784 points
+    data = data / 255.0  # Normalize
+    return data
+
 # Task 1 - Q5
 @app.post("/predict")
 async def predict(my_file: UploadFile):
     contents = await my_file.read()  # Read the file in bytes
-    with io.BytesIO(contents) as f:
-        data_point = plt.imread(f)
-    data_point = np.mean(data_point, axis=2)  # Convert to grayscale
-    data_point = data_point.reshape(
-        784
-    )  # Convert it to a serialized array of length 784
+    data_point = format_image(contents)
     model = load_model_weights(model_path)
     digit = predict_digit(model, data_point)
     return {"digit": digit}
